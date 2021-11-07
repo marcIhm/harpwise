c1bbb6d1fae6ab6c90ba51098bf916fad00a832c
Marc Ihm <marc@ihm.name>
2021-11-07 22:33:17 +0100

    trying

--- install-dependencies-on-ubuntu.sh
+++ install-dependencies-on-ubuntu.sh
@@ -6,17 +6,12 @@
 
 set -v
 
-
 # install packages one after the other to what they install as dependencies
 sudo apt-get install -y wget
 sudo apt-get install -y ruby
 sudo apt-get install -y figlet
 sudo apt-get install -y alsa-utils
-sudo echo "Europe/Zurich" > /etc/timezone 
-#sudo apt-get install dialog apt-utils
-#echo "tzdata    tzdata/Areas    select  Europe" | sudo debconf-set-selections
-#echo "tzdata    tzdata/Zones/Europe select  Berlin" | sudo debconf-set-selections
-sudo apt-get install --no-install-recommends -y aubio-tools
+sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y aubio-tools
 sudo apt-get install -y sox
 sudo apt-get install -y gnuplot-nox
 sudo apt-get install -y gcc
