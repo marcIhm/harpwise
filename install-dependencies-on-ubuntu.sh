2bcd5d3f636ed3d11ec08b7502ea0fe646effc96
Marc Ihm <marc@ihm.name>
2021-11-07 21:20:10 +0100

    testing install script

--- install-dependencies-on-ubuntu.sh
+++ install-dependencies-on-ubuntu.sh
@@ -9,20 +9,21 @@ set -v
 docker run -it -v $HOME/harp_scale_trainer:/root/harp_scale_trainer ubuntu bash
 
 # install packages one after the other to what they install as dependencies
-apt-get install --no-install-recommends -y sudo
-sudo apt-get install --no-install-recommends -y wget
-sudo apt-get install --no-install-recommends -y ruby
-sudo apt-get install --no-install-recommends -y figlet
-sudo apt-get install --no-install-recommends -y alsa-utils
-sudo apt-get install --no-install-recommends -y aubio-tools
-sudo apt-get install --no-install-recommends -y sox
-sudo apt-get install --no-install-recommends -y gnuplot-nox
-sudo apt-get install --no-install-recommends -y gcc
-sudo apt-get install --no-install-recommends -y make
-sudo apt-get install --no-install-recommends -y less
-sudo apt-get install --no-install-recommends -y ruby-dev
-sudo apt-get install --no-install-recommends -y libffi-dev
+sudo apt-get install -y wget
+sudo apt-get install -y ruby
+sudo apt-get install -y figlet
+sudo apt-get install -y alsa-utils
+sudo apt-get install -y aubio-tools
+sudo apt-get install -y sox
+sudo apt-get install -y gnuplot-nox
+sudo apt-get install -y gcc
+sudo apt-get install -y make
+sudo apt-get install -y less
+sudo apt-get install -y ruby-dev
+sudo apt-get install -y libffi
+sudo apt-get install -y libffi-dev
 sudo gem install sys-proctable
+cp /var/lib/gems/2.7.0/extensions/x86_64-linux/2.7.0/ffi-1.15.4/mkmf.log .
 
 # invoke as a test
 ./harp_scale_trainer
