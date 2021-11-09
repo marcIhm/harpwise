aa2b30e8d7390df0322f943329983f3240ecb313
Marc Ihm <marc@ihm.name>
2021-11-09 21:35:38 +0100

    installer for dependencies on fedora

---
+++ install/install-dependencies-on-fedora.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+set -v
+
+sudo yum install -y ruby
+sudo yum install -y figlet
+sudo yum install -y alsa-utils
+sudo yum install -y aubio
+sudo yum install -y sox
+sudo yum install -y gnuplot-minimal
+sudo yum install -y gcc
+sudo yum install -y make
+sudo yum install -y less
+sudo yum install -y ruby-devel
+sudo yum install -y libffi
+sudo yum install -y libffi-devel
+sudo gem install sys-proctable
--- install/install-dependencies-on-ubuntu.sh
+++ install/install-dependencies-on-ubuntu.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/bash
 
-# Install dependencies for harp_scale_trainer; the trainer itself needs no installation
-# but should rather be started directly from its directory.
-# See tests/installer for a way to test this script in a container
-
 set -v
 
-# install packages one after the other to what they install as dependencies
-sudo apt-get install -y wget
 sudo apt-get install -y ruby
 sudo apt-get install -y figlet
 sudo apt-get install -y alsa-utils
