d5a5406f89d5476f2aa681ccb6a127c4a47092c0
Marc Ihm <marc@ihm.name>
2021-12-07 10:58:52 +0100

    backlight in frequence gauge

--- lib/get_hole.rb
+++ lib/get_hole.rb
@@ -100,7 +100,7 @@ def get_hole lambda_issue, lambda_good_done, lambda_skip, lambda_comment, lambda
                              semi2freq_et(semi_ref - 2),
                              semi2freq_et(semi_ref),
                              semi2freq_et(semi_ref + 2)) {|ok,idx| idx}
-        print "\e[#{hit ? 92 : 31}m"
+        print ( hit  ?  "\e[92m\e[48;5;236m"  :  "\e[31m" )
         do_figlet dots, 'smblock', 'fixed:' + just_dots_long
       else
         print "\e[2m"
