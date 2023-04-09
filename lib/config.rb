de56f8075dddca1f6eba92cde752ca02b4a54d0f
Marc Ihm <marc@ihm.name>
2023-04-09 17:17:33 +0200

    Revert "avoid lagging info after wait during screen resize"
    This reverts commit 3efaf43acaac6cea746c0569da53b3e7a42e5dcb.

--- lib/config.rb
+++ lib/config.rb
@@ -9,7 +9,6 @@ def set_global_vars_early
   $word_re ='[[:alnum:]][-_:/\.[:alnum:]]*'
   $lagging_freqs_message_ts = 0
   $lagging_freqs_lost = 0
-  $lagging_expected = false
   $total_freqs = 0
 
   # two more entries will be set in find_and_check_dirs
