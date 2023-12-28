cdf7b01e0a8745ac65ea0adeb803f66c448a5be6
Marc Ihm <marc@ihm.name>
2023-12-28 22:21:49 +0100

    better installation instructions

---
+++ install/install-dependencies-on-macos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+set -v
+
+brew install aubio
+brew install coreutils
+brew install figlet
+brew install ruby
+brew install sox
+brew install toilet
--- install/install-dependencies-on-ubuntu.sh
+++ install/install-dependencies-on-ubuntu.sh
@@ -2,10 +2,10 @@
 
 set -v
 
-sudo apt-get install -y ruby
-sudo apt-get install -y figlet
-sudo apt-get install -y toilet
+sudo apt install -y ruby
+sudo apt install -y figlet
+sudo apt install -y toilet
 # The option --preserve-env below is needed (why ?) to test this script within a container
 sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y aubio-tools
-sudo apt-get install -y sox
-sudo apt-get install -y libsox-fmt-mp3 
+sudo apt install -y sox
+sudo apt install -y libsox-fmt-mp3 
