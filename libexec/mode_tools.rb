d611f36ab110149a398daa37202b107e9d17b750
Marc Ihm <marc@ihm.name>
2024-04-14 08:20:40 +0200

    wording

--- libexec/mode_tools.rb
+++ libexec/mode_tools.rb
@@ -531,16 +531,17 @@ def tool_diag
 
   puts <<~end_of_intro
   Harpwise uses the excellent program sox (aka rec aka play) under the
-  hood.  Normally, sox works just great, but on some system the
-  configuration of sox might have flaws.
+  hood.  Normally, sox works just great, but it relies on correct
+  configuration of your sound system on correct settings and operation
+  of your sound hardware.
 
   Therefore, this assistant will invoke sox (or rec or play, both are
   links to sox) in a typical way, giving you a chance to:
 
-    - Verify that sox (and therefore harpwise) can reord and play
+    - Verify that sox (and therefore harpwise) can record and play
       sounds
     - Easily spot any warnings or error messages, that might appear;
-      these will probably be \e[0;101min color\e[0m
+      these will be in \e[0;101mred\e[0m
 
   end_of_intro
 
@@ -548,7 +549,8 @@ def tool_diag
   FileUtils.rm file if File.exist?(file)
   print "\e[?25l"  ## hide cursor
 
-  puts_underlined 'Recording sound', '-'
+  puts
+  puts_underlined 'Recording sound'
   cmd_rec = if $testing
               "sleep 100"
             else
@@ -575,14 +577,15 @@ def tool_diag
   puts "Press any key to start: "
   drain_chars
   one_char
-  print "Recording started for 3 secs.\n\e[0;101m"
+  print "\n\e[32mRecording started for 3 secs.\e[0m\n\e[0;101m"
   rec_pid = Process.spawn cmd_rec
   sleep 3
   puts "\e[0m\e[K\nDone.\n\n"
   Process.kill('HUP', rec_pid)
   Process.wait(rec_pid)
 
-  puts_underlined 'Replaying sound', '-'
+  puts
+  puts_underlined 'Replaying sound'
   cmd_play = if $testing
               "sleep 3"
             else
@@ -594,7 +597,7 @@ def tool_diag
 
     #{cmd_play}
 
-  replay the sound, that has just been recorded.
+  to replay the sound, that has just been recorded.
 
   You should:
 
@@ -607,48 +610,59 @@ def tool_diag
   puts "Press any key to start: "
   drain_chars
   one_char
-  print "Replay started, 3 secs expected.\n\e[0;101m"
+  print "\n\e[32mReplay started, 3 secs expected.\e[0m\n\e[0;101m"
   rec_pid = Process.spawn cmd_play
   Process.wait(rec_pid)
   puts "\e[0m\e[K\nDone.\n\n"
 
-  puts_underlined 'Some hints on troubleshooting'
-  puts "If you noticed any problems or errors or warnings,\nthere are some hints, that might help."
-  puts "Type 'y' to read those hints (and anything else to end): "
+  puts
+  puts_underlined 'Get hints on troubleshooting sox ?'
+  puts <<~end_of_however
+  In many cases problems can be traced to incorrect settings,
+  e.g. of your sound hardware (volume turned up ?).
+
+  However, if you saw error messages in \e[0;101mred\e[0m,
+  the problem might be with the configuration of sox.
+  In that case, there are some hints, that might help.
+  end_of_however
+  
+  puts "\nType 'y' to read those hints (and anything else to end): "
   drain_chars
   if one_char == 'y'
-    puts <<~end_of_guide
+    puts "\n\e[32mSome hints:\e[0m\n"
+    puts <<end_of_guide
 
-    sox-Errors, which mention "no default audio device" or "encode
-    0-bit Unknown or not applicable" can sometimes be solved by
-    setting and exporting the environment variable AUDIODRIVER to a
-    suitable value.
+  sox-Errors, which mention "no default audio device" or "encode
+  0-bit Unknown or not applicable" can sometimes be solved by
+  setting and exporting the environment variable AUDIODRIVER to a
+  suitable value.
 
-    sox shows possible values for this when invoked without arguments;
-    just search for the line 'AUDIO DEVICE DRIVERS'. Possible values
-    might be 'alsa oss ossdsp pulseaudio' (linux) or 'coreaudio'
-    (macOS).
+  sox shows possible values for this when invoked without arguments;
+  just search for the line 'AUDIO DEVICE DRIVERS'. Possible values
+  might be 'alsa oss ossdsp pulseaudio' (linux) or 'coreaudio'
+  (macOS).
 
-    So e.g. on linux setting and exporting AUDIODRIVER=alsa might
-    help.
-
-
-    If this is not enough to solve the problem, you may also set
-    AUDIODEV to a suitable value, which however must be understood by
-    your audio driver (as specified by AUDIODRIVER).
-
-    As a linux example lets assume, that you have set AUDIODRIVER=alsa
-    above. Then, setting AUDIODEV=hw:0 in addition (which will inform
-    alsa about the device to use) might work. Note, that for macOS
-    most surely different values will be needed.
+  So e.g. on linux setting and exporting AUDIODRIVER=alsa might
+  help.
 
 
-    Other options necessary for sox might be passed through the
-    environment variable SOX_OPTS. See the man-page of sox for
-    details; also see the documentation of your respective audio
-    driver, e.g. alsa (for linux) or coreaudio (for macOS).
+  If this is not enough to solve the problem, you may also set
+  AUDIODEV to a suitable value, which however must be understood by
+  your audio driver (as specified by AUDIODRIVER).
 
-    end_of_guide
+  As a linux example lets assume, that you have set AUDIODRIVER=alsa
+  above. Then, setting AUDIODEV=hw:0 in addition (which will inform
+  alsa about the device to use) might work. Note, that for macOS
+  most surely different values will be needed.
+
+
+  Other options necessary for sox might be passed through the
+  environment variable SOX_OPTS. See the man-page of sox for
+  details; also see the documentation of your respective audio
+  driver, e.g. alsa (for linux) or coreaudio (for macOS).
+end_of_guide
+  else
+    puts "\n\e[32mNo hints requested.\e[0m"
   end
   
   puts "\nDiagnosis done.\n\n"  
